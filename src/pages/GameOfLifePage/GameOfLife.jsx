d5326d5a2ba1ff1c1065c119a20e8bf667df35b7
Stephan Loh <sloh4137@gmail.com>
2019-11-29 02:46:45 +0000

    Add fob to top

--- src/pages/GameOfLifePage/GameOfLife.jsx
+++ src/pages/GameOfLifePage/GameOfLife.jsx
@@ -7,14 +7,15 @@ import useWindowDimensions from "hooks/useWindowDimensions.jsx";
 import { useUniverse } from "hooks/useUniverse.jsx";
 import { CELL_SIZE } from "types/exports";
 
-import Fab from '@material-ui/core/Fab';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import PauseIcon from '@material-ui/icons/Pause';
+import Fab from "@material-ui/core/Fab";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import PauseIcon from "@material-ui/icons/Pause";
 
 import GridContainer from "components/Grid/GridContainer.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 import Menu from "./Menu";
 import Board from "./Board";
+import { GridList } from "@material-ui/core";
 
 function GameOfLife(props) {
     const { width, height } = useWindowDimensions();
@@ -34,15 +35,20 @@ function GameOfLife(props) {
                 justify="center"
                 alignItems="center"
             >
-                <Menu>
-                    <h1 align="center">{count}</h1>
-                    <Button onClick={() => setIsPaused(true)}>PAUSE</Button>
-                    <Button onClick={() => setIsPaused(false)}>START</Button>
-                </Menu>
+                <GridContainer justify="center">
+                    <Menu>
+                        <h1 align="center">{count}</h1>
+                        <Button onClick={() => setIsPaused(true)}>PAUSE</Button>
+                        <Button onClick={() => setIsPaused(false)}>
+                            START
+                        </Button>
+                    </Menu>
+                    <Fab size="small" onClick={() => setIsPaused(!isPaused)}>
+                        {isPaused ? <PlayArrowIcon /> : <PauseIcon />}
+                    </Fab>
+                </GridContainer>
+
                 <Board universe={state.universe} dispatch={dispatch}></Board>
-                <Fab onClick={() => setIsPaused(!isPaused)}>
-                    {isPaused ? <PlayArrowIcon /> : <PauseIcon />}
-                </Fab>
             </GridContainer>
         </>
     );
