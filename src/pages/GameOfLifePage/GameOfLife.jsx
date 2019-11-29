344f1c76fe776329ee19f38bff8199b1264e9c5d
Stephan Loh <sloh4137@gmail.com>
2019-11-29 01:59:23 +0000

    Move components to menu and add pause/play button

--- src/pages/GameOfLifePage/GameOfLife.jsx
+++ src/pages/GameOfLifePage/GameOfLife.jsx
@@ -7,8 +7,13 @@ import useWindowDimensions from "hooks/useWindowDimensions.jsx";
 import { useUniverse } from "hooks/useUniverse.jsx";
 import { CELL_SIZE } from "types/exports";
 
+import Fab from '@material-ui/core/Fab';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
+
 import GridContainer from "components/Grid/GridContainer.jsx";
 import Button from "components/CustomButtons/Button.jsx";
+import Menu from "./Menu";
 import Board from "./Board";
 
 function GameOfLife(props) {
@@ -16,22 +21,30 @@ function GameOfLife(props) {
     const numRow = Math.floor(height / CELL_SIZE) - 2;
     const numCol = Math.floor(width / CELL_SIZE);
 
-    const { setIsPaused, state, dispatch, count } = useUniverse(
+    const { isPaused, setIsPaused, state, dispatch, count } = useUniverse(
         numRow,
         numCol,
-        0.3,
+        0.3
     );
 
     return (
-        <GridContainer direction="column" justify="center" alignItems="center">
-            <h1 align="center">{count}</h1>
-            <GridContainer justify="center" alignItems="center">
-                <Button onClick={() => setIsPaused(true)}>PAUSE</Button>
-                <Button onClick={() => setIsPaused(false)}>START</Button>
+        <>
+            <GridContainer
+                direction="column"
+                justify="center"
+                alignItems="center"
+            >
+                <Menu>
+                    <h1 align="center">{count}</h1>
+                    <Button onClick={() => setIsPaused(true)}>PAUSE</Button>
+                    <Button onClick={() => setIsPaused(false)}>START</Button>
+                </Menu>
+                <Board universe={state.universe} dispatch={dispatch}></Board>
+                <Fab onClick={() => setIsPaused(!isPaused)}>
+                    {isPaused ? <PlayArrowIcon /> : <PauseIcon />}
+                </Fab>
             </GridContainer>
-            
-            <Board universe={state.universe} dispatch={dispatch}></Board>
-        </GridContainer>
+        </>
     );
 }
 
